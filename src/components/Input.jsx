cfe79bbabf3f1f3a13fb14e8547c8bf3bd8dcbaa
himanshurajput8 <rajputhimanshu913@gmail.com>
2025-05-12 10:06:09 +0530

    responsive code

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -5,21 +5,9 @@ const Input = ({ type = 'text', label, value, onChange, name, placeholder }) => 
     <div>
       {label && <label htmlFor={name}>{label}</label>}
       {type === 'checkbox' ? (
-        <input
-          type="checkbox"
-          id={name}
-          name={name}
-          checked={value}
-          onChange={(e) => onChange(e.target.checked)}
-        />
+        <input type="checkbox" id={name} name={name} checked={value} onChange={(e) => onChange(e.target.checked)}/>
       ) : (
-        <input
-          type={type}
-          id={name}
-          value={value}
-          onChange={(e) => onChange(e.target.value)}
-          placeholder={placeholder}
-        />
+        <input type={type} id={name} value={value} onChange={(e) => onChange(e.target.value)} placeholder={placeholder}/>
       )}
     </div>
   );
